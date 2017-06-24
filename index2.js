ad67f2b4658ebbac9231a48dfc4cfd02ca0ea719
ElsieHsieh <8chingen8@gmail.com>
2017-06-24 10:28:20 +0800

    災情類別14減成9，鄉鎮長條改成縣市長條

--- index2.js
+++ index2.js
@@ -40,13 +40,13 @@
     function draw() {
 
         var csv = d3.dsv(",", "text/csv;charset=big5");
-        d3.csv("nfa_data.csv", function(data) {
+        csv("nfa_data.csv", function(data) {
 
             var timeAllparse = d3.time.format("%Y-%m-%dT%H:%M:%S").parse,
                 dateformat = d3.time.format("%Y/%m/%d"),
                 timeformat = d3.time.format("%H:%M");
 
-            
+
             data.forEach(function(d) {
                 d.parseTime = timeAllparse(d.CaseDT);
                 d.date = dateformat(d.parseTime);
@@ -54,39 +54,67 @@
                 d.geo1 = d.WGS84_lat + "," + d.WGS84_lon;
 
                 var distype = d["DisasterMainType"];
-                var debris = 0 , flood = 0 , tree = 0 , ad = 0; 
-                var caracci = 0 , environment = 0 , fire = 0;
-                var building = 0 , hydraulic = 0 , other = 0;
-                var road = 0 , bridge = 0 , train_hsr_mrt = 0 , infrastructure = 0;
+                var debris = 0,
+                    flood = 0,
+                    tree = 0,
+                    ad = 0;
+                var caracci = 0,
+                    environment = 0,
+                    fire = 0;
+                var building = 0,
+                    hydraulic = 0,
+                    other = 0;
+                var road = 0,
+                    bridge = 0,
+                    train_hsr_mrt = 0,
+                    infrastructure = 0;
                 if (distype.indexOf("土石災情") > -1) {
                     debris = debris + 1;
-                }if (distype.indexOf("積淹水災情") > -1) {
+                }
+                if (distype.indexOf("積淹水災情") > -1) {
                     flood = flood + 1;
-                }if (distype.indexOf("路樹災情") > -1) {
+                }
+                if (distype.indexOf("路樹災情") > -1) {
                     tree = tree + 1;
-                }if (distype.indexOf("廣告招牌災情") > -1) {
-                    ad = ad + 1;
-                }if (distype.indexOf("其他災情") > -1) {
+                }
+                if (distype.indexOf("廣告招牌災情") > -1) {
                     other = other + 1;
-                }if (distype.indexOf("道路、隧道災情") > -1) {
+                    //ad = ad + 1;
+                }
+                if (distype.indexOf("其他災情") > -1) {
+                    other = other + 1;
+                }
+                if (distype.indexOf("道路、隧道災情") > -1) {
                     road = road + 1;
-                }if (distype.indexOf("橋梁災情") > -1) {
+                }
+                if (distype.indexOf("橋梁災情") > -1) {
                     bridge = bridge + 1;
-                }if (distype.indexOf("鐵路、高鐵及捷運災情") > -1) {
+                }
+                if (distype.indexOf("鐵路、高鐵及捷運災情") > -1) {
                     train_hsr_mrt = train_hsr_mrt + 1;
-                }if (distype.indexOf("建物毀損") > -1) {
-                    building = building + 1;
-                }if (distype.indexOf("水利設施災害") > -1) {
+                }
+                if (distype.indexOf("建物毀損") > -1) {
+                    other = other + 1;
+                    //building = building + 1;
+                }
+                if (distype.indexOf("水利設施災害") > -1) {
                     hydraulic = hydraulic + 1;
-                }if (distype.indexOf("車輛及交通事故") > -1) {
-                    caracci = caracci + 1;
-                }if (distype.indexOf("環境污染") > -1) {
-                    environment = environment + 1;
-                }if (distype.indexOf("火災") > -1) {
-                    fire = fire + 1;
-                }if (distype.indexOf("民生、基礎設施災情") > -1) {
+                }
+                if (distype.indexOf("車輛及交通事故") > -1) {
+                    other = other + 1;
+                    //caracci = caracci + 1;
+                }
+                if (distype.indexOf("環境污染") > -1) {
+                    other = other + 1;
+                    //environment = environment + 1;
+                }
+                if (distype.indexOf("火災") > -1) {
+                    other = other + 1;
+                    //fire = fire + 1;
+                }
+                if (distype.indexOf("民生、基礎設施災情") > -1) {
                     infrastructure = infrastructure + 1;
-                }          
+                }
                 d.debris1 = debris;
                 d.flood1 = flood;
                 d.tree1 = tree;
@@ -98,12 +126,12 @@
                 d.fire1 = fire;
                 d.building1 = building;
                 d.hydraulic1 = hydraulic;
-                
+
                 d.road1 = road;
                 d.bridge1 = bridge;
                 d.train_hsr_mrt1 = train_hsr_mrt;
                 d.infrastructure1 = infrastructure;
-                
+
 
             });
 
@@ -112,71 +140,95 @@
             var ndxGroupAll = ndx.groupAll();
 
             var geo1Dim = ndx.dimension(function(d) {
-                return d["geo1"]; }); //更改
+                return d["geo1"];
+            }); //更改
             var geo1Group = geo1Dim.group().reduceCount(); //更改
 
             var countyDim = ndx.dimension(function(d) {
-                return d["CityName"]; });
+                return d["CityName"];
+            });
             var townDim = ndx.dimension(function(d) {
-                return d["TownName"]; });
+                return d["TownName"];
+            });
             var townDisastersGroup = townDim.group().reduceCount(function(d) {
-                return d["DisasterMainType"]; }); //更改
+                return d["DisasterMainType"];
+            }); //更改
             var countyDisastersGroup = countyDim.group().reduceCount(function(d) {
-                return d.Flood1 + d.Landslide1 + d.Traffic1; }); //更改
+                //return d.landslide1 + d.rock1 + d.subgrade1 + d.block1 + d.Lateral1;
+                return d["DisasterMainType"];
+            });
             var townIdDim = ndx.dimension(function(d) {
-                return d["TOWN_ID"]; }); //更改
+                return d["TOWN_ID"];
+            }); //更改
 
             var timedim = ndx.dimension(function(d) {
-                return d.parseTime; });
+                return d.parseTime;
+            });
             var hourdim = ndx.dimension(function(d) {
-                return d3.time.hour(d.parseTime); });
+                return d3.time.hour(d.parseTime);
+            });
             var minTime = timedim.bottom(1)[0].parseTime;
             var maxTime = timedim.top(1)[0].parseTime;
 
             var disastertypes = ndx.dimension(function(d) {
-                return d["DisasterMainType"]; });
+                return d["DisasterMainType"];
+            });
             var disastertypesGroup = disastertypes.group().reduceCount();
             var debrisGroup = hourdim.group().reduceSum(function(d) {
-                return d.debris1; });
+                return d.debris1;
+            });
             var floodGroup = hourdim.group().reduceSum(function(d) {
-                return d.flood1; });
+                return d.flood1;
+            });
             var treeGroup = hourdim.group().reduceSum(function(d) {
-                return d.tree1; });
+                return d.tree1;
+            });
             var adGroup = hourdim.group().reduceSum(function(d) {
-                return d.ad1; });
+                return d.ad1;
+            });
             var otherGroup = hourdim.group().reduceSum(function(d) {
-                return d.other1; });
+                return d.other1;
+            });
 
             var caracciGroup = hourdim.group().reduceSum(function(d) {
-                return d.caracci1; });
+                return d.caracci1;
+            });
             var environmentGroup = hourdim.group().reduceSum(function(d) {
-                return d.environment1; });
+                return d.environment1;
+            });
             var fireGroup = hourdim.group().reduceSum(function(d) {
-                return d.fire1; });
+                return d.fire1;
+            });
             var buildingGroup = hourdim.group().reduceSum(function(d) {
-                return d.building1; });
+                return d.building1;
+            });
             var hydraulicGroup = hourdim.group().reduceSum(function(d) {
-                return d.hydraulic1; });
+                return d.hydraulic1;
+            });
 
             var roadGroup = hourdim.group().reduceSum(function(d) {
-                return d.road1; });
+                return d.road1;
+            });
             var bridgeGroup = hourdim.group().reduceSum(function(d) {
-                return d.bridge1; });
+                return d.bridge1;
+            });
             var train_hsr_mrtGroup = hourdim.group().reduceSum(function(d) {
-                return d.train_hsr_mrt1; });
+                return d.train_hsr_mrt1;
+            });
             var infrastructureGroup = hourdim.group().reduceSum(function(d) {
-                return d.infrastructure1; });
-     
+                return d.infrastructure1;
+            });
+
             //var testfileter = disastertypes.dimension(function (d) {return d.tree1;    });
             //var testfileterGroup = testfileter.groupAll();
-           
 
-             var colorScale = d3.scale.ordinal()
-                .domain(["土石災情", "其他災情", "積淹水災情", "路樹災情", "廣告招牌災情", "道路、隧道災情", "橋梁災情", "鐵路、高鐵及捷運災情", "建物毀損", "水利設施災害","車輛及交通事故", "環境污染", "火災", "民生、基礎設施災情"])
-                .range(["#cbbeb5", "#ffbe4f", "#fe5757", "#4b86b4", "#0ea7b5", "#9e9e9e","#85bdde","#63ace5","#6bd2db","#fe8181","#32ab9f","#3b5998","#e8702a","#0c457d"]);
 
-            var countycolor = d3.scale.ordinal().range(["#08589e","#2b8cbe", "#4eb3d3", "#7bccc4", "#a8ddb5", "#ccebc5"]);
-            
+            var colorScale = d3.scale.ordinal()
+                .domain(["土石災情", "其他災情", "積淹水災情", "路樹災情", "廣告招牌災情", "道路、隧道災情", "橋梁災情", "鐵路、高鐵及捷運災情", "建物毀損", "水利設施災害", "車輛及交通事故", "環境污染", "火災", "民生、基礎設施災情"])
+                .range(["#cbbeb5", "#ffbe4f", "#fe5757", "#4b86b4", "#0ea7b5", "#9e9e9e", "#85bdde", "#63ace5", "#6bd2db", "#fe8181", "#32ab9f", "#3b5998", "#e8702a", "#0c457d"]);
+
+            var countycolor = d3.scale.ordinal().range(["#08589e", "#2b8cbe", "#4eb3d3", "#7bccc4", "#a8ddb5", "#ccebc5"]);
+
 
             //cluster map - leaflet
             var MKmarker = dc_leaflet.markerChart("#map")
@@ -219,14 +271,13 @@
                 .dimension(townDim)
                 .group(townDisastersGroup, "Disasters")
                 .labelOffsetX(-45)
-                .colors(function(townDim) {
-                    return countycolor(townDim);
+                .colors(function(countyDim) {
+                    return countycolor(countyDim);
                 })
                 .elasticX(true)
                 .controlsUseVisibility(true)
-                .ordering(function(d) {
-                    return townDim;
-                })
+                .ordering(function (d) { return -d.value; })
+      .title(function (d) { return d.value; })
                 .rowsCap(5);
 
             //filter and total count number
@@ -245,21 +296,21 @@
                 });
 
             // time bar chart
- 
-                // .group(otherGroup, "其他災情")
-                // .stack(debrisGroup, "土石災情")
-                // .stack(floodGroup, "積淹水災情")
-                // .stack(treeGroup, "路樹災情")
-                // .stack(adGroup, "廣告招牌災情")
-                // .stack(roadGroup, "道路、隧道災情")
-                // .stack(bridgeGroup, "橋梁災情")
-                // .stack(train_hsr_mrtGroup, "鐵路、高鐵及捷運災情")
-                // .stack(buildingGroup, "建物毀損")
-                // .stack(hydraulicGroup, "水利設施災害")
-                // .stack(infrastructureGroup, "民生、基礎設施災情")
-                // .stack(caracciGroup, "車輛及交通事故")
-                // .stack(environmentGroup, "環境污染")
-                // .stack(fireGroup, "火災")
+
+            // .group(otherGroup, "其他災情")
+            // .stack(debrisGroup, "土石災情")
+            // .stack(floodGroup, "積淹水災情")
+            // .stack(treeGroup, "路樹災情")
+            // .stack(adGroup, "廣告招牌災情")
+            // .stack(roadGroup, "道路、隧道災情")
+            // .stack(bridgeGroup, "橋梁災情")
+            // .stack(train_hsr_mrtGroup, "鐵路、高鐵及捷運災情")
+            // .stack(buildingGroup, "建物毀損")
+            // .stack(hydraulicGroup, "水利設施災害")
+            // .stack(infrastructureGroup, "民生、基礎設施災情")
+            // .stack(caracciGroup, "車輛及交通事故")
+            // .stack(environmentGroup, "環境污染")
+            // .stack(fireGroup, "火災")
 
 
             var timechart = dc.barChart("#dis_time")
@@ -283,7 +334,8 @@
                 .stack(environmentGroup, "環境污染")
                 .stack(fireGroup, "火災")
                 .colors(function(DisasterMainType) {
-                    return colorScale(DisasterMainType); })
+                    return colorScale(DisasterMainType);
+                })
                 .elasticY(true)
                 .renderHorizontalGridLines(true)
                 .mouseZoomable(false)
@@ -291,7 +343,8 @@
                 .xAxisLabel("Date")
                 .centerBar(true)
                 .xUnits(function(d) {
-                    return 100 })
+                    return 100
+                })
                 .brushOn(true)
                 .xAxis().tickFormat(d3.time.format('%m/%d %H:%M'));
 
@@ -332,49 +385,42 @@
                 "scrollY": 300,
                 "scrollCollapse": true,
                 "scroller": true,
-                "columnDefs": [
-                    {
+                "columnDefs": [{
                         targets: 0,
-                        data: function (d) {
+                        data: function(d) {
                             return d.CityName;
                         },
-                    },
-                    {
+                    }, {
                         targets: 1,
-                        data: function (d) {
+                        data: function(d) {
                             return d.TownName;
                         },
-                    },
-                    {
+                    }, {
                         targets: 2,
-                        data: function (d) {
+                        data: function(d) {
                             return d.date;
                         },
                         type: 'date',
-                    },
-                    {
+                    }, {
                         targets: 3,
-                        data: function (d) {
-                 
+                        data: function(d) {
+
                             return d.tt;
                         },
-                    },
-                    {
+                    }, {
                         targets: 4,
-                        data: function (d) {
+                        data: function(d) {
                             return d.DisasterMainType;
                         },
-                    },
-                    {
+                    }, {
                         targets: 5,
-                        data: function (d) {
-                            
+                        data: function(d) {
+
                             return d.DisasterSubType;
                         },
-                    },
-                    {
+                    }, {
                         targets: 6,
-                        data: function (d) {
+                        data: function(d) {
                             return d.CaseDescription;
                         },
                     }
@@ -382,20 +428,20 @@
                 ]
             });
 
-          function RefreshTable() {
-              dc.events.trigger(function () {
-                  alldata = townDim.top(Infinity);
-                  datatable.clear();
-                  datatable.rows.add(alldata);
-                  datatable.draw();
-              });
-          }
+            function RefreshTable() {
+                dc.events.trigger(function() {
+                    alldata = townDim.top(Infinity);
+                    datatable.clear();
+                    datatable.rows.add(alldata);
+                    datatable.draw();
+                });
+            }
 
-          for (var i = 0; i < dc.chartRegistry.list().length; i++) {
-        var chartI = dc.chartRegistry.list()[i];
-        chartI.on("filtered", RefreshTable);
-    }
-    RefreshTable();
-    dc.renderAll();
+            for (var i = 0; i < dc.chartRegistry.list().length; i++) {
+                var chartI = dc.chartRegistry.list()[i];
+                chartI.on("filtered", RefreshTable);
+            }
+            RefreshTable();
+            dc.renderAll();
         });
     }
